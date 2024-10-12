9f17c877c09a61038d8611b050a4da2209d1ce7b
Andrew Mills <andrewmills@knowwwhow.com>
2024-10-12 19:45:23 -0400

    Create MorseCode.js
    initial commit

---
+++ MorseCode.js
@@ -0,0 +1,88 @@
+/*
+ *  Name: MorseCode.js
+ *  Purpose: JS Object for encoding/decoding Morse Code messages
+ *  Example(s):
+ *    myMorse.textToMorse("Hello"); // returns .... . .-.. .-.. --- 
+ *    myMorse.morseToText(".... . .-.. .-.. ---"); // returns HELLO
+ */
+function MorseCode(){
+
+    // map for encoding Morse
+    const morseMap = {
+      // Letters
+      'A': '.-',    'B': '-...',  'C': '-.-.',  'D': '-..',   'E': '.',
+      'F': '..-.',  'G': '--.',   'H': '....',  'I': '..',    'J': '.---',
+      'K': '-.-',   'L': '.-..',  'M': '--',    'N': '-.',    'O': '---',
+      'P': '.--.',  'Q': '--.-',  'R': '.-.',   'S': '...',   'T': '-',
+      'U': '..-',   'V': '...-',  'W': '.--',   'X': '-..-',  'Y': '-.--',
+      'Z': '--..',
+
+      // Numbers
+      '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....',
+      '6': '-....', '7': '--...', '8': '---..', '9': '----.', '0': '-----',
+
+      // Punctuation
+      '.': '.-.-.-',  // Period
+      ',': '--..--',  // Comma
+      '?': '..--..',  // Question mark
+      "'": '.----.',  // Apostrophe
+      '!': '-.-.--',  // Exclamation mark
+      '/': '-..-.',   // Slash
+      '(': '-.--.',   // Open parenthesis
+      ')': '-.--.-',  // Close parenthesis
+      '&': '.-...',   // Ampersand
+      ':': '---...',  // Colon
+      ';': '-.-.-.',  // Semicolon
+      '=': '-...-',   // Equals sign
+      '+': '.-.-.',   // Plus
+      '-': '-....-',  // Hyphen/Dash
+      '_': '..--.-',  // Underscore
+      '"': '.-..-.',  // Quotation marks
+      '$': '...-..-', // Dollar sign
+      '@': '.--.-.'   // At sign
+    };
+
+    // Reversed map for decoding Morse
+    const reverseMorseMap = {};
+    for (let letter in morseMap) {
+        reverseMorseMap[morseMap[letter]] = letter;
+    }
+
+    function textToMorse(txtPlain){
+        let results = '';
+
+        txtPlain
+            .toUpperCase()
+            .split('')
+            .map( character => {
+                if ( character === ' ' ) {
+                    results += '   ';
+                }
+                else {
+                    results += morseMap[ character ];
+                    results += ' ';
+                }
+            })
+        
+        return results;
+    }
+
+    function morseToText(txtMorse){
+        let results = '';
+
+        txtMorse
+            .split(' ')
+            .map( character => {
+                if ( reverseMorseMap[ character ] !== undefined )
+                    results += reverseMorseMap[ character ];
+                else 
+                    results += ' ';
+            });
+        
+        return results.replaceAll('   ', ' ');
+    }
+    
+    return {
+        textToMorse, morseToText
+    }
+}
